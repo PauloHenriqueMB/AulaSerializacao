12fa6ffe6a30071096cfd3c6f38dc45c50eea1a1
Paulo Henrique <p.h.moreirabarbosa@hotmail.com>
2016-05-10 20:20:44 -0300

    Comentarios removidos

--- src/br/univel/TesteGravacao.java
+++ src/br/univel/TesteGravacao.java
@@ -15,19 +15,17 @@ public class TesteGravacao {
 		cloriginal.setId(2);
 		cloriginal.setNome("Paulo Barbosa");
 		
-		File file = new File("arquivo.dat");//O objeto file aponta para um determinado arquivo (referência) que esteja em uma determinada partição
+		File file = new File("arquivo.dat");
 		
 		try (FileOutputStream fos = new FileOutputStream(file); 
-				ObjectOutputStream oos = new ObjectOutputStream(fos)){//A atribuição das variaveis dos recursos dentro dos () define que este sera fechado automaticamente
+				ObjectOutputStream oos = new ObjectOutputStream(fos)){
 
-			oos.writeObject(cloriginal);//Envia os atributos do objeto 'cloriginal' para o 'oos' e este grava no 'fos'
+			oos.writeObject(cloriginal);
 			
-		}catch(FileNotFoundException e){//Irá mostrar uma excessão caso o arquivo não seja encontrado
-			
+		}catch(FileNotFoundException e){
 			e.printStackTrace();
 			
-		}catch(IOException e){//Irá mostrar uma excessão caso ocorra alguma objeção na compilação do código
-			
+		}catch(IOException e){
 			e.printStackTrace();
 			
 		}
